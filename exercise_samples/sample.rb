d3652eab568f70b91ab4d94d501e22294737893d
Rebekah <rebekah_7567@hotmail.co.uk>
2018-11-16 16:27:10 +0000

    complete hash question

---
+++ exercise_samples/sample.rb
@@ -0,0 +1,16 @@
+
+
+
+
+
+
+
+veggies = ['peas', 'leeks', 'sprouts']
+veggies.push('lettuce')
+ veggies
+
+
+
+books = { "fiction" => "funny", "horror" => "scary", "non-fiction" => "factual"}
+books.delete("horror")
+p books
